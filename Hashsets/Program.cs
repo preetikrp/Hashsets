eb1080be6e85f5b8d1254f078d8ec63c758f76f8
preetikrp <preetikrp@gmail.com>
2017-07-11 11:27:15 -0500

    HashSet

---
+++ Hashsets/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> showroom = new HashSet<string>()
+            {
+                "Mercedes-Benz", "Audi", "BMW", "Porsche"
+
+
+
+            };
+
+            Console.WriteLine(showroom.Count);
+            Console.ReadLine();
+
+            showroom.Add("Audi");
+            foreach (string item in showroom)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+            }
+
+
+
+            HashSet<string> UsedLot = new HashSet<string>() {
+                "Honda", "Lexus"
+
+            };
+            foreach (string item in UsedLot)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+                showroom.UnionWith(UsedLot);
+                showroom.Remove("Honda");
+                
+                Console.WriteLine(showroom.Count);
+                Console.ReadLine();
+            }
+
+            HashSet<string> Junkyard = new HashSet<string>()
+            {
+                "BMW", "Nissan Altima", "Toyota Camry"
+
+            };
+
+            Console.WriteLine(Junkyard.Count);
+            Console.ReadLine();
+            Junkyard.IntersectWith(Junkyard);
+            foreach (string item in showroom)
+            {
+                Console.WriteLine(item);
+                Console.ReadLine();
+                Junkyard.Remove("BMW");
+
+
+
+            }
+
+            foreach (string item in showroom)
+            {
+
+                Console.WriteLine(item);
+                Console.ReadLine();
+
+            }
+        }
+
+    }
+}
+            
+
+            
+
+
+
+
+
+    
+    
+
